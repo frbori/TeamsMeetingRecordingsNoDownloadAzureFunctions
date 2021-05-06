28b5725a2eb5395d0d5c16004f587d0d382a2a3f
Francesco Bori <frbori@microsoft.com>
2021-05-06 17:49:37 +0200

    Added helper functions

--- profile.ps1
+++ profile.ps1
@@ -27,11 +27,100 @@ function GetTeamWebsiteUrl() {
         [string][Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$TeamID,
         [string][Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$AccessToken
     )
+    Write-Debug "Retrieving Team '$teamDisplayName' SharePoint site url."
     $graphUrl = "https://graph.microsoft.com/v1.0/groups/$TeamID/sites/root?`$select=webUrl"
     $headers = @{
         "Content-Type"  = "application/json"
         "Authorization" = "Bearer $AccessToken"
     }
-    $response = Invoke-RestMethod -Uri $graphUrl -Headers $headers -Method Get -ContentType "application/json"
+    try {
+        $response = Invoke-RestMethod -Uri $graphUrl -Headers $headers -Method Get -ContentType "application/json"
+    }
+    catch {
+        $outcome = "Team error."
+        $details = "An error while retrieving the SharePoint Team Site url occurred."
+        Write-Error "$outcome $details"
+        throw $_.Exception
+    }
     return $response.webUrl
+}
+function GetRestrictedView() {
+    param
+    (
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$PnPConnection,
+        [string][Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$SPSiteUrl,
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$SPDocumentLibrary,
+        [switch]$PrivateChannel
+    )
+    $restrictedView = $null
+    try {
+        Write-Debug "Retrieving Restricted View SharePoint permission level."
+        $roleDefs = Get-PnPRoleDefinition -Connection $PnPConnection
+        $restrictedView = $roleDefs | Where-Object { $_.RoleTypeKind -eq "RestrictedReader" }
+        if ($null -eq $restrictedView) {
+            try {
+                $spDocumentsListId = $SPDocumentLibrary.Id
+                $uri = "$SPSiteUrl/_api/web/Lists(@a1)/GetItemById(@a2)/GetSharingInformation?@a1=%27%7B$spDocumentsListId%7D%27&@a2=%271%27&`$Expand=sharingLinkTemplates"
+                Invoke-PnPSPRestMethod -Method Post -Url $uri -ContentType "application/json" -Content @{} | Out-Null
+    
+                $roleDefs = Get-PnPRoleDefinition -Connection $PnPConnection
+                $restrictedView = $roleDefs | Where-Object { $_.RoleTypeKind -eq "RestrictedReader" }
+                $restrictedView.GetType() | Out-Null
+            }
+            catch {
+                if ($null -eq $PrivateChannel) {
+                    $outcome = "Team error."
+                    $details = "An error while triggering Restricted View permission level occurred."
+                    Write-Error "$outcome $details"
+                    throw $_.Exception
+                }
+                else {
+                    $outcome = "Channel error."
+                    $details = "An error while retrieving SharePoint Role Definitions for private channel occurred. "
+                    $exceptions = $_.Exception.Message
+                    Write-Warning "$outcome $details $exceptions"
+                }
+            }
+        }
+    }
+    catch {
+        $outcome = "Team error."
+        $details = "An error while handling the custom permission level occurred."
+        Write-Error "$outcome $details"
+        throw $_.Exception
+    }   
+    return $restrictedView
+}
+function GetSPDocumentsLibrary() {
+    param
+    (
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]$PnPConnection,
+        [switch]$PrivateChannel
+    )
+    $spLibrary = $null
+    try {
+        $documentsListName = "Documents"
+        Write-Debug "Retrieving SharePoint '$documentsListName' document library."
+        $spLibrary = Get-PnPList -Identity $documentsListName -Connection $PnPConnection
+        if ($null -eq $spLibrary) {
+            $documentsListName = "Documenti"
+            Write-Debug "Retrieving SharePoint '$documentsListName' document library."
+            $spLibrary = Get-PnPList -Identity $documentsListName -ErrorAction Stop -Connection $PnPConnection
+        }
+    }
+    catch {
+        if ($null -eq $PrivateChannel) {
+            $outcome = "Team error."
+            $details = "An error while retrieving the 'Documents' document library occurred."
+            Write-Error "$outcome $details"
+            throw $_.Exception
+        }
+        else {
+            $outcome = "Channel error."
+            $details = "An error while retrieving the 'Documents' document library for private channel occurred."
+            $exceptions = $_.Exception.Message
+            Write-Error "$outcome $details $exceptions"
+        }
+    }
+    return $spLibrary
 }
